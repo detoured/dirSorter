5e3f6d49aa5b93963e8e20a8be6150d004851821
detoured <jcdetoured@gmail.com>
2026-04-15 22:15:31 +0300

    get exts from txt

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Organizes a directory by moving files into seperate sub directories that are sor
 
 Usage format:
 
-> Sort files using default extensions and directory:
+> Sort files using default extensions and directories:
 ```
 python3 main.py <path_to_dir>
 ```
@@ -13,14 +13,16 @@ python3 main.py <path_to_dir>
 python3 main.py <path_to_dir> <-ae> <dir> <ext1>
 ```
 
-> Add a custom extentions to a custom directory
+> Add custom extentions to a custom directory
 ```
 python3 main.py <path_to_dir> <-ae> <dir> <ext1,ext2,ext3>
 ```
 
-> Add a custom extentions to a custom directories
+> Add custom extentions to custom directories
 ```
 python3 main.py <path_to_dir> <-ae> <dir> <ext1,ext2,ext3> <-ae> <dir2> <ext4,ext5,ext6> 
 ```
 
-*\ If you want to add a custom extension to a default directory, you use the same command as you would for a custom directory.
+* To alter the default settings to your liking, edit the defualt_values.txt file. Use this format: name:ext1,ext2,ext3
+
+* If you want to add a custom extension to a default directory, you use the same command as you would for a custom directory.
--- main.py
+++ main.py
@@ -5,40 +5,59 @@ import shutil
 
 
 def main():
-    extentions = {
-    "images" : ["jpg","jpeg","jpe","png"],
-    "videos" : ["mp4","mov", "avi", "mkv", "wmv"],
-    "documents" : ["txt","pdf"]
-    }
-
+    extentions = assign_defaults()
+    
     handle_argv(extentions)
 
     path = sys.argv[1]
-    contents = os.listdir(f"{path}")
+    content = os.listdir(f"{path}")
 
-    files = assign_files(contents,extentions)
+    files = assign_files(content,extentions)
 
-    creare_dirs(path,contents,extentions,files)
+    creare_dirs(path,content,extentions,files)
 
     move_files(path,files)
 
+def assign_defaults():
+        with open("default_values.txt", "r") as file:
+            lines = {}
+            current_line = -1
+            extentions = {}
+            content = file.read()
+            for charI in range(0,len(content)):
+                if charI == 0 or content[charI] == "\n":
+                    current_line += 1
+                    lines[current_line] = ""
+                    if content[charI] == "\n":
+                        continue
+                lines[current_line] += content[charI]
+
+            for ext in range(0,len(lines)):
+                extentions[lines[ext].split(":")[0]] = lines[ext].split(":")[1].split(",")
+            return extentions
+
 def handle_argv(extentions):
     if len(sys.argv) < 2:
-        print("Please provide a path to a dir")
+        print("Please provide a path to a directory")
         sys.exit(1)
     
     for arg in range (0,len(sys.argv)):
         if sys.argv[arg] == "-ae":
+            add_ext(extentions, arg)
+
+def add_ext(extentions, arg):
+        try:
             if sys.argv[arg+1] in extentions:
                     extentions[sys.argv[arg+1]] += (sys.argv[arg+2]).split(",")
             else:
                 extentions[sys.argv[arg+1]] = sys.argv[arg+2]
-    
+        except:
+            print("Invalid [-ae] arguements.")
 
-def assign_files(contents, extentions):
+def assign_files(content, extentions):
     files = {}
     
-    for file in contents:
+    for file in content:
         for ext in extentions:
             if file.rsplit(".",1)[1] in extentions[ext]:
                 if ext not in files:
@@ -46,13 +65,14 @@ def assign_files(contents, extentions):
                 files[ext].append(file)
     return files
 
-def creare_dirs(path,contents,extentions,files):
+def creare_dirs(path,content,extentions,files):
     for ext in extentions:
-        if ext not in contents and ext in files:
+        if ext not in content and ext in files:
                 Path(f"{path}/{ext}").mkdir()
 
 def move_files(path, files):
     for file_type in files:
         for file in files[file_type]:
             shutil.move(f"{path}/{file}",f"{path}/{file_type}")
+
 main()
